0eca9bcb465986e982b15f423c2a4297e24fd9d9
FrancoisBdB <fr.moffatt@gmail.com>
2021-06-30 15:50:25 -0400

    test3

--- src/main/java/com/fm/mavenprojectjenkins/AppCtr.java
+++ src/main/java/com/fm/mavenprojectjenkins/AppCtr.java
@@ -4,5 +4,6 @@ package com.fm.mavenprojectjenkins;
 public class AppCtr {
     public static void main(String[] args) {
         System.out.println("test2");
+        System.out.println("test3");
     }
 }
